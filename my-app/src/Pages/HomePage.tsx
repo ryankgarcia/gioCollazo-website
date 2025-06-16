a2f1e8495c3040ec8c8edb27d1d921077e66856d
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-16 16:23:03 -0700

    added photos and minor styling adjustments.

--- my-app/src/Pages/HomePage.tsx
+++ my-app/src/Pages/HomePage.tsx
@@ -1,10 +1,11 @@
-
+import './HomePageLayout.css'
 
 export function HomePage(){
     return (
         <div className="home-container">
-         <h2>Home Page</h2>
-         <img className="home-image" src='/src/assets/daytrip-outdoor.jpg' />
+         <h2>Client's I've Styled</h2>
+         <img className="home-image" src='/src/assets/daytrip-outdoor.jpg' alt='styled-woman' />
+         <img className='home-image' src='/src/assets/LC-pink-fit.jpg' alt='stylist-woman' />
         </div>
     )
 }
