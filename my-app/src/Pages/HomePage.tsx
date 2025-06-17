a17d38a626c7f0ced95f8c8541b607d06de6356b
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-16 18:05:55 -0700

    pushing to github.

--- my-app/src/Pages/HomePage.tsx
+++ my-app/src/Pages/HomePage.tsx
@@ -4,8 +4,8 @@ export function HomePage(){
     return (
         <div className="home-container">
          <h2>Client's I've Styled</h2>
-         <img className="home-image" src='/src/assets/daytrip-outdoor.jpg' alt='styled-woman' />
-         <img className='home-image' src='/src/assets/LC-pink-fit.jpg' alt='stylist-woman' />
+         <img className="home-image" src='/daytrip-outdoor.jpg' alt='styled-woman' />
+         <img className='home-image' src='/LC-pink-fit.jpg' alt='stylist-woman' />
         </div>
     )
 }
