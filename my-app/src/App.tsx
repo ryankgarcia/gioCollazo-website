a81ae2c86c340f13c7c7b1822c2036ae30dd4f07
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-18 12:20:09 -0700

    committing changes to new branch.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,10 @@
+
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from './Pages/HomePage'
 import { AboutMe } from './Pages/AboutMe'
-import { NavBar } from './Components/NavBar'
 import { Testimonials } from './Pages/Testimonials'
+import { WorkWithMe } from './Pages/WorkWithMe'
+import { NavBar } from './Components/NavBar'
 import './index.css'
 
 
@@ -14,6 +16,7 @@ function App() {
         <Route index element={<HomePage />}/>
         <Route path='/about' element={<AboutMe />}/>
         <Route path='/testimonials' element={<Testimonials />}/>
+        <Route path='/workwithme' element={<WorkWithMe />}/>
         </Route>
     </Routes>
   )
