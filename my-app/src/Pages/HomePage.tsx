6e7ce8e1527027859439d3f3ff3e147188471b24
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-23 23:58:39 -0700

    formatting the page to look like Gio's example image.

--- my-app/src/Pages/HomePage.tsx
+++ my-app/src/Pages/HomePage.tsx
@@ -3,10 +3,14 @@ import './HomePageLayout.css'
 export function HomePage(){
     return (
         <div className="home-container">
+            <div className='top-third'>
          <h2 className='clients-styled'>Client's I've Styled</h2>
+         </div>
          <img className="home-image" src='/daytrip-outdoor.jpg' alt='styled-woman' />
          <img className='home-image' src='/LC-pink-fit.jpg' alt='stylist-woman' />
+         <div className='bottom-third'>
          <h2 className='clients-styled'>Brands I've Worked With</h2>
+         </div>
         </div>
     )
 }
