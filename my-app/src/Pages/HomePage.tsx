3f6761a4d1ea97aa045c6ecba074864fe07c7e93
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-24 02:02:54 -0700

    turned layout into rows and columns.

--- my-app/src/Pages/HomePage.tsx
+++ my-app/src/Pages/HomePage.tsx
@@ -4,7 +4,7 @@ export function HomePage(){
     return (
         <div className="home-container">
             <div className='row'>
-            <div className='top-third'>
+            <div className='column-third'>
          <h2 className='clients-styled'>Client's I've Styled</h2>
          </div>
          </div>
@@ -12,7 +12,7 @@ export function HomePage(){
          <img className="home-image" src='/daytrip-outdoor.jpg' alt='styled-woman' />
          <img className='home-image' src='/LC-pink-fit.jpg' alt='stylist-woman' />
          </div>
-         <div className='bottom-third'>
+         <div className='column-third'>
          <h2 className='clients-styled'>Brands I've Worked With</h2>
          </div>
         </div>
