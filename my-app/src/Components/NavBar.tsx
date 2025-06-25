36881d0da77688632929b8e23c53b5bbd637599d
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-25 12:17:30 -0700

    Installed prettier and fixed package.json.

--- my-app/src/Components/NavBar.tsx
+++ my-app/src/Components/NavBar.tsx
@@ -1,24 +1,49 @@
-
-import { NavLink, Outlet } from "react-router-dom";
-import { IoIosBowtie } from "react-icons/io";
+import { NavLink, Outlet } from 'react-router-dom';
+import { IoIosBowtie } from 'react-icons/io';
 import './NavBar.css';
 
 export function NavBar() {
-    return (
-        <div>
-     <h2 className='navBarHeader h2'> <IoIosBowtie />  StyledByGio  <IoIosBowtie /> </h2>
-     <div className="allLinks">
-     <NavLink to='/' className={({ isActive }) =>
-    isActive ? 'nav-link active' : 'nav-link'}><span className="home-link">Home</span>
-     </NavLink >
-     <NavLink to='/about' className={({ isActive }) => 
-     isActive ? 'nav-link active': 'nav-link'}><span className="aboutMe-link">About Me</span></NavLink>
-     <NavLink to='/testimonials' className={({ isActive }) => 
-     isActive ? 'nav-link active': 'nav-link'}><span className="testimonials-link">Testimonials</span></NavLink>
-     <NavLink to='/contact' className={({ isActive }) => 
-     isActive ? 'nav-link active': 'nav-link'}><span className="contact-link">Contact</span></NavLink>
-     </div>
-     <Outlet />
-     </div>
-    )    
+  return (
+    <div>
+      <h2 className="navBarHeader h2">
+        {' '}
+        <IoIosBowtie /> StyledByGio <IoIosBowtie />{' '}
+      </h2>
+      <div className="allLinks">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          <span className="home-link">Home</span>
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          <span className="aboutMe-link">About Me</span>
+        </NavLink>
+        <NavLink
+          to="/testimonials"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          <span className="testimonials-link">Testimonials</span>
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          <span className="contact-link">Contact</span>
+        </NavLink>
+      </div>
+      <Outlet />
+    </div>
+  );
 }
--- my-app/src/Pages/Contact.tsx
+++ my-app/src/Pages/Contact.tsx
@@ -1,11 +1,11 @@
-import { WorkWithMe } from '../Components/WorkWithMe - Form'
-import './Contact.css'
+import { WorkWithMe } from '../Components/WorkWithMe - Form';
+import './Contact.css';
 
 export function ContactPage() {
-    return (
-        <div>
-        <h2 className='starting-point'>This is the Contact Me page . . .</h2>
-        <WorkWithMe />
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="starting-point">This is the Contact Me page . . .</h2>
+      <WorkWithMe />
+    </div>
+  );
 }
