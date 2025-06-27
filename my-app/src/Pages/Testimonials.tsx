600e05ceb45430d1e5f4c48e53246907409bc031
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-27 13:26:21 -0700

    added testimonails text to the page.

--- my-app/src/Pages/Testimonials.tsx
+++ my-app/src/Pages/Testimonials.tsx
@@ -2,8 +2,98 @@ import './Testimonials.css';
 
 export function Testimonials() {
   return (
-    <div>
-      <h2>Testimonials Page ! ! !</h2>
+    <div className="testimonial-container">
+      <div className="testimonial-row">
+        <div className="testimonial-column-full">
+          <h2>Testimonials Page ! ! !</h2>
+        </div>
+      </div>
+      <div className="testimonial-row">
+        <div className="testimonial-column-full">
+          <img
+            src="/testimonials-images/louie-portrait-image.jpeg"
+            alt="Louie-testimonial1"
+            className="testimonial-portrait-img"
+          />
+          <p>
+            Working with Gio has been nothing short of amazing. I’ve had the
+            pleasure of collaborating with him on both personal and professional
+            styling needs, and every experience has exceeded my expectations.
+            Gio is the perfect balance of professionalism and personality—always
+            bringing a fun, positive energy that makes the entire process
+            enjoyable from start to finish. He has an incredible eye for style,
+            but more importantly, he knows how to make you feel confident and
+            comfortable, even in moments when you need that extra boost. Whether
+            it was a planned event or a last-minute request, Gio always came
+            through with thoughtful styling and reliable support. His work
+            consistently made me feel like the best version of myself, and
+            that’s a gift not every stylist can offer. I recommend Gio
+            wholeheartedly—he’s not only talented and dependable, but also kind,
+            approachable, and fairly priced. If you're looking for someone who
+            truly cares about helping you feel and look your best, Gio is your
+            guy. Louie - Husband, Father, Business Owner -- Los Angeles{' '}
+          </p>
+        </div>
+      </div>
+      <div className="testimonial-row">
+        <div className="testimonial-column-full">
+          <img
+            src="/testimonials-images/Sophia-portrait-image.jpeg"
+            alt="Sophia-testimonial2"
+            className="testimonial-portrait-img"
+          />
+          <p>
+            I’ve been working with Gio for years, and he’s come through for me
+            in so many clutch moments — even last-minute fittings before major
+            events. He’s not only flexible and kind, but he truly listens.
+            Whether I need a look that fits a specific vibe, flatters a certain
+            area, or aligns with the energy of the venue, he gets it and makes
+            it happen without pushback. As part of a DJ duo, I love that he
+            curates outfits that complement both me and Alex — stylish,
+            cohesive, but never too matchy. Gio genuinely cares about his
+            clients and it shows in everything he does. Sophia- DJ, Business
+            Owner, Socialite -- Los Angeles
+          </p>
+        </div>
+      </div>
+      <div className="testimonial-row">
+        <div className="testimonial-column-full">
+          <img
+            src="/testimonials-images/alex-portrait-image.jpeg"
+            alt="Alex-testimonial3"
+            className="testimonial-portrait-img"
+          />
+          <p>
+            Working with Gio has been a game-changer. He’s created custom looks
+            for both Sophia and me that hit the vibe perfectly every time — from
+            high-energy performances to more elevated, themed events. He’s easy
+            to work with, thoughtful, and really pays attention to detail. We
+            always feel confident and comfortable in his pieces, and he finds
+            that sweet spot where we coordinate well without looking too
+            overdone. Can’t recommend him enough. Alex - DJ, Producer, Socialite
+            -- Los Angeles{' '}
+          </p>
+        </div>
+      </div>
+      <div className="testimonial-row">
+        <div className="testimonial-column-full">
+          <img
+            src="/testimonials-images/nicole-portrait-image.jpeg"
+            alt="Nicole-testimonial4"
+            className="testimonial-portrait-img"
+          />
+          <p>
+            I have worked with Gio a few times for vacations I was taking. Every
+            time, he had me looking and feeling cute. I appreciate the fact he
+            listens, and works with me on what im comfortable with, and
+            considers my feedback. He makes me feel comfortable when working
+            with him. It’s always fun working with him. And it doesn’t feel like
+            it’s business, but more like a friend helping me. He’s always my go
+            to stylist. Nicole - Model, Entrepreneur, Make up Artist -- Dallas,
+            Texas{' '}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
