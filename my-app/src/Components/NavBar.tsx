994a13dcaefafb472af16fc6a61b8cea0f061790
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-29 21:03:06 -0700

    adding sliding transition to the navBar. opens and closes using useState to show all navLinks.

--- my-app/src/Components/NavBar.tsx
+++ my-app/src/Components/NavBar.tsx
@@ -1,63 +1,54 @@
 import { NavLink, Outlet } from 'react-router-dom';
-import { IoIosBowtie } from 'react-icons/io';
+// import { IoIosBowtie } from 'react-icons/io';
+import { BiMenu } from 'react-icons/bi';
 import './NavBar.css';
+import { useState } from 'react';
+
+// type Props {
+// }
 
 export function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpen(): void {
+    setIsOpen((prev) => !prev);
+  }
+
   return (
     <div>
-      <h2 className="navBarHeader h2">
-        <IoIosBowtie /> StyledByGio <IoIosBowtie />{' '}
-      </h2>
-      <div className="allLinks">
-        <div className="nav-row">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            <span className="home-link">Home</span>
-          </NavLink>
-        </div>
-        <div className="nav-row">
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            <span className="aboutMe-link">About</span>
-          </NavLink>
-        </div>
-        <div className="nav-row">
-          <NavLink
-            to="/services"
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            <span className="aboutMe-link">Services</span>
-          </NavLink>
-        </div>
-        <NavLink
-          to="/testimonials"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          <span className="testimonials-link">Testimonials</span>
-        </NavLink>
-        <div className="nav-row">
-          <NavLink
-            to="/contact"
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            <span className="contact-link">Contact</span>
-          </NavLink>
-        </div>
+      <div onClick={handleOpen} className="navBarHeader h2">
+        <BiMenu /> StyledByGio
       </div>
+      {/* <h2 className="navBarHeader h2">
+        <IoIosBowtie /> StyledByGio <IoIosBowtie />{' '}
+      </h2> */}
+      {isOpen && (
+        <div className={`allLinks ${isOpen ? 'open' : ''}`}>
+          <div className="nav-row">
+            <NavLink to="/">
+              <span className="home-link">Home</span>
+            </NavLink>
+          </div>
+          <div className="nav-row">
+            <NavLink to="/about">
+              <span className="aboutMe-link">About</span>
+            </NavLink>
+          </div>
+          <div className="nav-row">
+            <NavLink to="/services">
+              <span className="aboutMe-link">Services</span>
+            </NavLink>
+          </div>
+          <NavLink to="/testimonials">
+            <span className="testimonials-link">Testimonials</span>
+          </NavLink>
+          <div className="nav-row">
+            <NavLink to="/contact">
+              <span className="contact-link">Contact</span>
+            </NavLink>
+          </div>
+        </div>
+      )}
       <Outlet />
     </div>
   );
