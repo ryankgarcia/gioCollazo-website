d5564e13bf0d1a0c709427f5ee3958a74c7d802b
Ryan Garcia <ryankgarcia22@gmail.com>
2025-06-30 11:55:40 -0700

    completed the navBar sliding menu effect.

--- my-app/src/Components/NavBar.tsx
+++ my-app/src/Components/NavBar.tsx
@@ -4,9 +4,6 @@ import { BiMenu } from 'react-icons/bi';
 import './NavBar.css';
 import { useState } from 'react';
 
-// type Props {
-// }
-
 export function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -22,33 +19,33 @@ export function NavBar() {
       {/* <h2 className="navBarHeader h2">
         <IoIosBowtie /> StyledByGio <IoIosBowtie />{' '}
       </h2> */}
-      {isOpen && (
-        <div className={`allLinks ${isOpen ? 'open' : ''}`}>
-          <div className="nav-row">
-            <NavLink to="/">
-              <span className="home-link">Home</span>
-            </NavLink>
-          </div>
-          <div className="nav-row">
-            <NavLink to="/about">
-              <span className="aboutMe-link">About</span>
-            </NavLink>
-          </div>
-          <div className="nav-row">
-            <NavLink to="/services">
-              <span className="aboutMe-link">Services</span>
-            </NavLink>
-          </div>
-          <NavLink to="/testimonials">
-            <span className="testimonials-link">Testimonials</span>
+      {/* {isOpen && ( */}
+      <div className={`allLinks ${isOpen ? 'open' : ''}`}>
+        <div className="nav-row">
+          <NavLink to="/">
+            <span className="home-link">Home</span>
           </NavLink>
-          <div className="nav-row">
-            <NavLink to="/contact">
-              <span className="contact-link">Contact</span>
-            </NavLink>
-          </div>
         </div>
-      )}
+        <div className="nav-row">
+          <NavLink to="/about">
+            <span className="aboutMe-link">About</span>
+          </NavLink>
+        </div>
+        <div className="nav-row">
+          <NavLink to="/services">
+            <span className="aboutMe-link">Services</span>
+          </NavLink>
+        </div>
+        <NavLink to="/testimonials">
+          <span className="testimonials-link">Testimonials</span>
+        </NavLink>
+        <div className="nav-row">
+          <NavLink to="/contact">
+            <span className="contact-link">Contact</span>
+          </NavLink>
+        </div>
+      </div>
+      {/* )} */}
       <Outlet />
     </div>
   );
