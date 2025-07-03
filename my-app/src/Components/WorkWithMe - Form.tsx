6b90e638bdddd35fce3916ba4da272d8472f7bf5
Ryan Garcia <ryankgarcia22@gmail.com>
2025-07-03 14:47:00 -0700

    added useState to capture input information on the form component.

--- my-app/src/Components/WorkWithMe - Form.tsx
+++ my-app/src/Components/WorkWithMe - Form.tsx
@@ -1,44 +1,100 @@
+import { useState, type FormEvent } from 'react';
 import './WorkWithMe.css';
 
 export function WorkWithMe() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [message, setMessage] = useState('');
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    console.log({ firstName, lastName, email, phoneNumber, message });
+  }
+
   return (
     <>
-      <form className="form-container">
+      <form
+        method="POST"
+        data-email=""
+        action=""
+        className="gform form-container"
+        onSubmit={handleSubmit}
+      >
         <div className="workWMe-row">
-          <label htmlFor="first-name">First Name (required)</label>
-          <input id="first-name" type="text" className="input-field" required />
+          <label htmlFor="first-name">
+            First Name (required)
+            <input
+              id="first-name"
+              type="text"
+              name="firstName"
+              className="input-field"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </label>
         </div>
         <div className="workWMe-row">
           <label htmlFor="last-name" id="LN-label-spacing-left">
             Last Name
+            <input
+              id="last-name"
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="input-field"
+            />
           </label>
-          <input id="last-name" type="text" className="input-field" />
         </div>
         <div className="workWMe-row">
-          <label htmlFor="email">Email (required)</label>
-          <input id="email" type="email" className="input-field" required />
+          <label htmlFor="email">
+            Email (required)
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="input-field"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
         </div>
         <div className="workWMe-row">
           <label htmlFor="phone-number" className="phone-number-label">
             Phone Number (required)
+            <input
+              id="phone-number"
+              type="text"
+              name="phoneNumber"
+              className="input-field"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required
+            />
           </label>
-          <input
-            id="phone-number"
-            type="text"
-            className="input-field"
-            required
-          />
         </div>
         <div className="workWMe-row">
-          <label htmlFor="message">Message</label>
+          <label htmlFor="message">
+            Message
+            <input
+              id="message"
+              type="textbox"
+              name="message"
+              className="input-field"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </label>
         </div>
-        <div className="workWMe-row">
-          <input id="message" type="textbox" className="input-field" />
-        </div>
-
         <div className="workWMe-row">
           <div className="workWMe-column-half">
-            <button className="form-submit-button">Submit</button>
+            <button type="submit" className="form-submit-button">
+              Submit
+            </button>
           </div>
         </div>
       </form>
