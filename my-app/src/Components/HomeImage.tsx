fd421bf03ecc72cd517d3197767b89ff6fc885ae
Ryan Garcia <ryankgarcia22@gmail.com>
2025-07-05 22:15:06 -0700

    saving homeImage component for lazy loading approach.

---
+++ my-app/src/Components/HomeImage.tsx
@@ -0,0 +1,20 @@
+import './HomeImage.css';
+
+export function HomeImage() {
+  const url: string[] = [
+    '/gio-w-shades.jpg',
+    '/starWars-themed.jpg',
+    '/LC-yeehaw.jpg',
+  ];
+
+  function selectPhoto(url: string[]): string {
+    const imgUrl: string = '';
+    for (let i = 0; i < url.length; i++) {}
+    return imgUrl;
+  }
+  return (
+    <>
+      <img />
+    </>
+  );
+}
