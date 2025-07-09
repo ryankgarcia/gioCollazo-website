2a059cc01b1e0a74191262d258a42a8236060f34
Ryan Garcia <ryankgarcia22@gmail.com>
2025-07-09 15:04:35 -0700

    added className to the Top10Looks component.

--- my-app/src/Components/Top10Looks.tsx
+++ my-app/src/Components/Top10Looks.tsx
@@ -4,22 +4,71 @@ interface topLooks {
   id: number;
   src: string;
   alt: string;
-  // className: string; // add the proper className attribute to each of these based on what was current
-  // at the time of the last deployed version
+  className: string; // add the proper className attribute to each of these based on what was current
 }
 
 export function Top10Looks() {
   const top10Picks: topLooks[] = [
-    { id: 1, src: '/blackDress-dressingRoom.jpg', alt: 'topLooksImage1' },
-    { id: 2, src: '/indoor-pinkDress-whitebkgd.jpg', alt: 'topLooksImage2' },
-    { id: 3, src: '/LC-black-and-yellow.jpg', alt: 'topLooksImage3' },
-    { id: 4, src: '/LC-indoor-coachella-fit3.jpg', alt: 'topLooksImage4' },
-    { id: 5, src: '/tealDress-blondeWoman.jpg', alt: 'topLooksImage5' },
-    { id: 6, src: '/black-redBlazer.jpg', alt: 'topLooksImage6' },
-    { id: 7, src: '/club-maverick-red-on-black.jpg', alt: 'topLooksImage7' },
-    { id: 8, src: '/LC-yeehaw.jpg', alt: 'topLooksImage8' },
-    { id: 9, src: '/pinkStyled.jpg', alt: 'topLooksImage9' },
-    { id: 10, src: '/redCarpet-tropicalDress.jpg', alt: 'topLooksImage10' },
+    {
+      id: 1,
+      src: '/blackDress-dressingRoom.jpg',
+      alt: 'topLooksImage1',
+      className: 'image-wrapper',
+    },
+    {
+      id: 2,
+      src: '/indoor-pinkDress-whitebkgd.jpg',
+      alt: 'topLooksImage2',
+      className: 'image-wrapper',
+    },
+    {
+      id: 3,
+      src: '/LC-black-and-yellow.jpg',
+      alt: 'topLooksImage3',
+      className: 'image-wrapper',
+    },
+    {
+      id: 4,
+      src: '/LC-indoor-coachella-fit3.jpg',
+      alt: 'topLooksImage4',
+      className: 'image-wrapper',
+    },
+    {
+      id: 5,
+      src: '/tealDress-blondeWoman.jpg',
+      alt: 'topLooksImage5',
+      className: 'image-wrapper',
+    },
+    {
+      id: 6,
+      src: '/black-redBlazer.jpg',
+      alt: 'topLooksImage6',
+      className: 'image-wrapper',
+    },
+    {
+      id: 7,
+      src: '/club-maverick-red-on-black.jpg',
+      alt: 'topLooksImage7',
+      className: 'image-wrapper',
+    },
+    {
+      id: 8,
+      src: '/LC-yeehaw.jpg',
+      alt: 'topLooksImage8',
+      className: 'image-wrapper',
+    },
+    {
+      id: 9,
+      src: '/pinkStyled.jpg',
+      alt: 'topLooksImage9',
+      className: 'image-wrapper',
+    },
+    {
+      id: 10,
+      src: '/redCarpet-tropicalDress.jpg',
+      alt: 'topLooksImage10',
+      className: 'image-wrapper',
+    },
   ];
 
   return (
@@ -30,7 +79,7 @@ export function Top10Looks() {
             key={image.id}
             src={image.src}
             alt={image.alt}
-            // className="image-wrapper"
+            className="image-wrapper"
           />
         ))
       ) : (
