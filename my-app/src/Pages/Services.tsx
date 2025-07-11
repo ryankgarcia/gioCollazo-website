c5f2ddabefc11fa14564d29a3193f4c51d53532c
Ryan Garcia <ryankgarcia22@gmail.com>
2025-07-10 17:26:16 -0700

    made changes to the header element on the contact page.

--- my-app/src/Pages/Services.tsx
+++ my-app/src/Pages/Services.tsx
@@ -4,25 +4,34 @@ import './Services.css';
 
 export function Services() {
   return (
-    <>
-      <h2>Services</h2>
-      <h3>A La Carte</h3>
+    <div className="services-container">
+      <h3 className="service-headerMain">A La Carte</h3>
       <p></p>
       <p></p>
-      <h3>👗 Closet Clean-Outs</h3>
+      <h3 className="service-subheader">👗 Closet Clean-Outs</h3>
       <p>
         Let’s detox your wardrobe and make space for who you are now. I’ll help
-        you sort through what to keep, toss, or tailor—while creating new
+        you sort through what to keep, toss, or tailor while creating new
         outfits with what you already own.
       </p>
       <p>No judgment. Just clarity, style, and a fresh perspective.</p>
       <p>Includes:</p>
-      <p>✅ </p>
-      <p>✅ </p>
-      <p>✅ </p>
-      <p>✅ </p>
-      <Link to="/contact">Book Your Session. . .</Link>
-      <h3>🛍️ Shopping with Gio</h3>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <Link to="/contact" className="book-me">
+        Book Your Session
+      </Link>
+      <h3 className="service-subheader">🛍️ Shopping with Gio</h3>
       <p>
         Your budget. Your vibe. Your glow-up. Whether it’s a full style overhaul
         or just a seasonal refresh, we’ll hit the stores together and try on
@@ -33,23 +42,53 @@ export function Services() {
         confidence. (2 hour-minimum)
       </p>
       <p>Includes:</p>
-      <p>✅ Body type education</p>
-      <p>✅ Styling tips as we shop</p>
-      <p>✅ Outfit building in real time</p>
-      <p>✅ Feel-good vibes all day</p>
       <p>
-        Ready for your wardrobe glow-up? Let's make it personal. Let's make it
-        fashion. 💅
+        <span className="emoji-large">✅</span> Body type education
       </p>
-      <Link to="/contact">Book Your Session. . .</Link>
-      <h3>✈️ Trips & Events Package 💃</h3>
+      <p>
+        <span className="emoji-large">✅</span> Styling tips as we shop
+      </p>
+      <p>
+        <span className="emoji-large">✅</span> Outfit building in real time
+      </p>
+      <p>
+        <span className="emoji-large">✅</span> Feel-good vibes all day
+      </p>
+      <p>Ready for your wardrobe glow-up? </p> <p>Let's make it personal.</p>
+      <p>Let's make it fashion. 💅</p>
+      <Link to="/contact" className="book-me">
+        Book Your Session
+      </Link>
+      <h3 className="service-subheader">✈️ Trips & Events 💃</h3>
+      <p>
+        Getting ready to travel somewhere new? Going to a special event and want
+        to dress to impress?
+      </p>
+      <p>
+        This is the most popular a la carte item amongst my clients for those
+        who are traveling abroad, going to a party, or a festival.
+      </p>
+      <p>
+        Let's get you all glam'ed up for your special occasion by helping you
+        understand your body's best features.
+      </p>
       <p>Includes:</p>
-      <p>✅ </p>
-      <p>✅ </p>
-      <p>✅ </p>
-      <p>✅ </p>
-      <Link to="/contact">Book Your Session. . .</Link>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <p>
+        <span className="emoji-large">✅</span>
+      </p>
+      <Link to="/contact" className="book-me">
+        Book Your Session
+      </Link>
       <Footer />
-    </>
+    </div>
   );
 }
