daede04f67b585eeaca328eca380ae7457d7ce97
Ryan Garcia <ryankgarcia22@gmail.com>
2025-07-30 20:53:57 -0700

    completed the flipping image on the home page for the behind the scenes component.

--- my-app/src/Components/BehindTheScenes.tsx
+++ my-app/src/Components/BehindTheScenes.tsx
@@ -6,7 +6,7 @@ interface imageElement {
   id: number;
   src: string;
   alt: string;
-  className: 'bts-card-1' | 'bts-card-2';
+  className: 'bts-card' | 'bts-card';
   backsideText: string;
   linkTo: string;
   button: string;
@@ -23,7 +23,7 @@ export function BehindTheScenes() {
       id: 1,
       src: '/BTS - outdoor-man-burgundy.jpg',
       alt: 'BTSImage1',
-      className: 'bts-card-1',
+      className: 'bts-card',
       backsideText: 'Red carpet independent viewing',
       linkTo: '/services',
       button: 'backside-btn',
@@ -33,7 +33,7 @@ export function BehindTheScenes() {
       id: 2,
       src: '/BTS - photoshoot.jpg',
       alt: 'BTSImage2',
-      className: 'bts-card-2',
+      className: 'bts-card',
       // className: 'bts-wrapper-2 bts-object-cover', // the commented out className's are the original before the flip effect
       backsideText: 'Teen vogue photo shoot',
       linkTo: '/services',
@@ -44,7 +44,7 @@ export function BehindTheScenes() {
       id: 3,
       src: '/BTS - quince-outside.jpg',
       alt: 'BTSImage3',
-      className: 'bts-card-1',
+      className: 'bts-card',
       // className: 'bts-wrapper-1 bts-object-fill',
       backsideText: 'Quinceñera photo shoot',
       linkTo: '/services',
@@ -55,7 +55,7 @@ export function BehindTheScenes() {
       id: 4,
       src: '/BTS - quince.jpg',
       alt: 'BTSImage4',
-      className: 'bts-card-2',
+      className: 'bts-card',
       // className: 'bts-wrapper-2 bts-object-fill',
       backsideText: 'Quinceñera photo shoot',
       linkTo: '/services',
@@ -66,7 +66,7 @@ export function BehindTheScenes() {
       id: 5,
       src: '/BTS-Gio-at-photoshoot.jpg',
       alt: 'BTSImage5',
-      className: 'bts-card-1',
+      className: 'bts-card',
       // className: 'bts-wrapper-1 bts-object-fill',
       backsideText: 'Featured Home Magazine',
       linkTo: '/services',
@@ -77,7 +77,7 @@ export function BehindTheScenes() {
       id: 6,
       src: '/BTS-Gio-desert-outdoor.jpg',
       alt: 'BTSImage6',
-      className: 'bts-card-2',
+      className: 'bts-card',
       // className: 'bts-wrapper-2 bts-object-cover',
       backsideText: 'On a movie set',
       linkTo: '/services',
@@ -88,7 +88,7 @@ export function BehindTheScenes() {
       id: 7,
       src: '/BTS-Gio-shoeReveal.jpg',
       alt: 'BTSImage7',
-      className: 'bts-card-1',
+      className: 'bts-card',
       // className: 'bts-wrapper-1 bts-object-fill',
       backsideText: 'Preparing for a wedding',
       linkTo: '/services',
@@ -99,7 +99,7 @@ export function BehindTheScenes() {
       id: 8,
       src: '/bts-LC-Gio-BlackDress.JPEG',
       alt: 'BTSImage8',
-      className: 'bts-card-2',
+      className: 'bts-card',
       // className: 'bts-wrapper-2 bts-object-fill',
       backsideText: 'Rave outfit fitting',
       linkTo: '/services',
@@ -110,7 +110,7 @@ export function BehindTheScenes() {
       id: 9,
       src: '/bts-shoppingExp.JPG',
       alt: 'BTSImage9',
-      className: 'bts-card-1',
+      className: 'bts-card',
       // className: 'bts-wrapper-1 bts-object-fill',
       backsideText: 'Personal Shopping Experience',
       linkTo: '/services',
@@ -122,36 +122,42 @@ export function BehindTheScenes() {
   return (
     <>
       {behindTheScenesImage.length > 0 ? (
-        behindTheScenesImage.map((image, index) => (
-          <div className={`bts-card ${image.className}`} key={image.id}>
-            <div className="bts-card-inner">
-              <div
-                className={`bts-card-front ${
-                  visibleStates[index] ? 'bts-fade-in' : 'bts-hidden'
-                }`}
-              >
-                <img
-                  ref={(element) => {
-                    imageRefs.current[index] = element;
-                  }}
-                  src={image.src}
-                  alt={image.alt}
-                  className={` bts-image ${image.fitStyle}`}
-                />
-              </div>
-              <div className="bts-card-back">
-                <div className="bts-row">
-                  <div className="bts-column-full">
-                    <p>{image.backsideText}</p>
+        behindTheScenesImage.map((image, index) => {
+          const shadowClass =
+            index % 2 === 0
+              ? 'bts-shadow-light-purple'
+              : 'bts-shadow-dark-purple';
+          return (
+            <div className={`bts-card ${image.className}`} key={image.id}>
+              <div className="bts-card-inner">
+                <div
+                  className={`bts-card-front ${shadowClass} ${
+                    visibleStates[index] ? 'bts-fade-in' : 'bts-hidden'
+                  }`}
+                >
+                  <img
+                    ref={(element) => {
+                      imageRefs.current[index] = element;
+                    }}
+                    src={image.src}
+                    alt={image.alt}
+                    className={` bts-image ${image.fitStyle}`}
+                  />
+                </div>
+                <div className="bts-card-back">
+                  <div className="bts-row">
+                    <div className="bts-column-full">
+                      <p>{image.backsideText}</p>
+                    </div>
+                    <Link to={image.linkTo} className={image.button}>
+                      Book Now
+                    </Link>
                   </div>
-                  <Link to={image.linkTo} className={image.button}>
-                    Book Now
-                  </Link>
                 </div>
               </div>
             </div>
-          </div>
-        ))
+          );
+        })
       ) : (
         <p>No images have loaded...</p>
       )}
