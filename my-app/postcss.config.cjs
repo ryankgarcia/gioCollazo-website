8b4d282610390c1d3b22d79fd13b1c560ba6cae8
Ryan Garcia <ryankgarcia22@gmail.com>
2025-08-09 22:15:22 -0700

    global media queries work in the footer component.

--- my-app/postcss.config.cjs
+++ my-app/postcss.config.cjs
@@ -1,36 +1,15 @@
-// import postcssPresetEnv from 'postcss-preset-env';
-
-// export default {
-//   plugins: [
-//     postcssPresetEnv({
-//       stage: 1,
-//       features: { 'custom-media-queries': true },
-//       importFrom: {
-//         customMedia: {
-//           '--tablet': '(min-width: 600px)',
-//           '--sm-desktop': '(min-width: 900px)',
-//           '--desktop': '(min-width: 1200px)',
-//           '--lg-desktop': '(min-width: 1536px)',
-//         },
-//       },
-//     }),
-//   ],
-// };
-
 module.exports = {
   plugins: [
-    require('postcss-preset-env')({
-      stage: 1,
-      features: { 'custom-media-queries': true },
+    require('postcss-import'),
 
-      importFrom: {
-        customMedia: {
-          '--tablet': '(min-width: 600px)',
-          '--sm-desktop': '(min-width: 900px)',
-          '--desktop': '(min-width: 1200px)',
-          '--lg-desktop': '(min-width: 1536px)',
-        },
-      },
+    require('@csstools/postcss-global-data')({
+      files: ['./src/styles/media.css'],
     }),
+
+    require('postcss-custom-media')({
+      preserve: false,
+    }),
+
+    require('autoprefixer'),
   ],
 };
