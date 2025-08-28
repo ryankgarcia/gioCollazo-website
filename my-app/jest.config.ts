53c409a3dcb9ce091e537e23b48d79e3ddaf9cd1
Ryan Garcia <ryankgarcia22@gmail.com>
2025-08-27 22:33:56 -0700

    updated jest config and setupTests to prepare for the last gradient scroll test setup.

--- my-app/jest.config.ts
+++ my-app/jest.config.ts
@@ -4,8 +4,10 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  resolver: 'ts-jest-resolver',
+  roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': [
+    '^.+\\.(ts|tsx)$': [
       'ts-jest',
       {
         useESM: true,
@@ -18,7 +20,19 @@ const config: Config.InitialOptions = {
 
   moduleNameMapper: {
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
+
+    '^@/(.*)$': '<rootDir>/src/$1',
+
+    '\\.(gif|ttf|eot|svg|png|jpe?g|webp)$': '<rootDir>/src/test/__mocks__/fileMock.cjs',
   },
+
+  transformIgnorePatterns: [
+    '/node_modules/(?!(lucide-react|class-variance-authority|@radix-ui|nanoid)/)',
+  ],
+
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+
+  moduleFileExtensions: ['ts','tsx','js','jsx','json']
 };
 
 export default config;
